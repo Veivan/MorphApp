e471e098693c4b36aa82fa71fdd700f803bceb5d
veivan <veivan@yandex.ru>
2017-12-01 16:54:57 +0300

    Начал делать dbServer

---
+++ dbMQserver/SQLiteConnector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dbMQserver
+{
+	class SQLiteConnector : IdbConnector
+	{
+		public long SaveLex(string word)
+		{
+			throw new NotImplementedException();
+		}
+
+		public long GetWord(string rword)
+		{
+			throw new NotImplementedException();
+		}
+
+		public long SavePhrase(long ph_id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public long SavePhraseContent(long ph_id, long w_id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ dbMQserver/ServerStart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dbMQserver
+{
+	class ServerStart
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
