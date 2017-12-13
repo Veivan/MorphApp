f7fdfc1f686a2e9253f19866ef4252e802b60c37
veivan <veivan@yandex.ru>
2017-12-13 12:33:31 +0300

    Добавил тесты для dbMQserver

---
+++ dbMQserverTests/SQLiteConnectorTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using dbMQserver;
+
+namespace dbMQserverTests
+{
+	[TestClass]
+	public class SQLiteConnectorTest
+	{
+		[TestMethod]
+		public void SaveLex_qq_Returned_graterThen0()
+		{
+			// arrange
+			SQLiteConnector dbConnector = SQLiteConnector.Instance;
+
+			// act
+			long res = dbConnector.SaveLex("qq");
+
+			// assert
+			Assert.AreNotEqual(0, res);
+		}
+
+		[TestMethod]
+		public void GetWord_qq_Returned_qq()
+		{
+			// arrange
+			SQLiteConnector dbConnector = SQLiteConnector.Instance;
+
+			// act
+			long res = dbConnector.GetWord("qq");
+
+			// assert
+			Assert.AreNotEqual(-1, res);
+		}
+	}
+}
