1dbc336cd556c0e4b26d0da9f5d183bdf732ac1e
Veivan <veivan@ya.ru>
2018-07-17 14:53:02 +0300

    Изменил свойства тестовых приложений на AnyCPU и тесты появились в обозревателе.

--- DirectDBconnector/Properties/DirectDBC.Designer.cs
+++ DirectDBconnector/Properties/DirectDBC.Designer.cs
@@ -12,7 +12,7 @@ namespace DirectDBconnector.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
     internal sealed partial class DirectDBC : global::System.Configuration.ApplicationSettingsBase {
         
         private static DirectDBC defaultInstance = ((DirectDBC)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new DirectDBC())));
